5f7918aefd5b3cd64bb7ad636a1bc0d393356e00
machikodesu <jobhinam@gmail.com>
2023-08-20 21:49:10 +0900

    javascriptの手数料計算と他の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,14 +14,14 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render 'new', status: :unprocessable_entity
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
+                                 :delivery_day_id, :image).merge(user_id: current_user.id)
   end
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,78 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  
   before do
-    @user = FactoryBot.create(:user) 
+    @user = FactoryBot.create(:user)
     @item = FactoryBot.build(:item, user: @user)
   end
-  
+
   describe '商品出品登録' do
     context '新規登録できる場合' do
       it '必須項目が全てあれば登録できる' do
         expect(@item).to be_valid
       end
     end
-  
+
     context '新規作成できない場合' do
-      it "nameが空では登録できない" do
+      it 'nameが空では登録できない' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
 
-    it 'descriptionが空では登録できない' do
-      @item.description = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Description can't be blank")
-    end
+      it 'descriptionが空では登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
 
-    it 'category_idがない場合は登録できない' do
-      @item.category_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
-    end
+      it 'category_idがない場合は登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
 
-    it 'delivery_fee_idがない場合は登録できない' do
-      @item.delivery_fee_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Delivery fee can't be blank", "Delivery fee is not a number", "Delivery fee must exist")
-    end
+      it 'delivery_fee_idがない場合は登録できない' do
+        @item.delivery_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank", 'Delivery fee is not a number',
+                                                      'Delivery fee must exist')
+      end
 
-    it 'prefecture_idがない場合は登録できない' do
-      @item.prefecture_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-    end
+      it 'prefecture_idがない場合は登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
 
-    it 'delivery_day_idがない場合は登録できない' do
-      @item.delivery_day_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Delivery day can't be blank", "Delivery day is not a number", "Delivery day must exist")
-    end
+      it 'delivery_day_idがない場合は登録できない' do
+        @item.delivery_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank", 'Delivery day is not a number',
+                                                      'Delivery day must exist')
+      end
 
-    it 'priceがない場合は登録できない' do
-      @item.price = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
-    it 'imageがない場合は登録できない' do
-      @item.image = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
-    it 'priceが10000000以上では登録できない' do
-      @item.price = 50000000
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
-    end
-    it 'priceが300未満では登録できない' do
-      @item.price = 299
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      it 'priceがない場合は登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'imageがない場合は登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'priceが10000000以上では登録できない' do
+        @item.price = 50_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it 'priceが300未満では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
     end
   end
-end
-
-
 end
