6960084a861bf2f16a101cdd3f5ac37c1f5eefe5
machikodesu <jobhinam@gmail.com>
2023-08-25 14:46:47 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,8 +26,6 @@ class ItemsController < ApplicationController
   def edit
     if @item.order.present? || current_user != @item.user
       redirect_to root_path
-    else
-      redirect_to root_path
     end
   end
 
