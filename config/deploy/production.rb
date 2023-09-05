4ec5f446e7e2b259d359cd05f9aa14c7a4c2a061
machikodesu <jobhinam@gmail.com>
2023-09-05 16:38:17 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '35.78.5.4', user: 'ec2-user', roles: %w{app db web}
+server '52.193.34.100', user: 'ec2-user', roles: %w{app db web}
